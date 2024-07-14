2ddf35ac81596f8879f2bc6bc8a86577354d69d7
Cristina Reyes <gpe.cristina.reyes@gmail.com>
2024-07-14 13:47:01 -0600

    Agrega clase tratador de errores

---
+++ foro-hub/src/main/java/alura/cursos/foro_hub/infra/errores/TratadorDeErrores.java
@@ -0,0 +1,36 @@
+package alura.cursos.foro_hub.infra.errores;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+@RestControllerAdvice
+public class TratadorDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity tratarError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity tratarError400(MethodArgumentNotValidException e){
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
+    public ResponseEntity<String> tratarErrorDuplicado(SQLIntegrityConstraintViolationException e){
+        String mensaje = "Violación de restricción de integridad: " + e.getMessage();
+        return ResponseEntity.badRequest().body(mensaje);
+    }
+
+    private record DatosErrorValidacion(String campo, String error){
+        public DatosErrorValidacion(FieldError error) {
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
